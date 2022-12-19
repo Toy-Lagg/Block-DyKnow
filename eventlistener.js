118dc61fba7597febcf4b88b5fc7ceb80ff22f00
Toy-Lagg <116599916+Toy-Lagg@users.noreply.github.com>
2022-12-19 13:22:47 -0500

    Create eventlistener.js

---
+++ eventlistener.js
@@ -0,0 +1,4 @@
+// Listen for clicks on the DyKnow Cloud extension button
+chrome.browserAction.onClicked.addListener(function() {
+  // This function will be executed when the button is clicked
+});
